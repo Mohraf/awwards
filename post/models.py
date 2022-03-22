f6eb03c254f362430be6e946b62cde112ee1fefb
mohraf <amosngok@gmail.com>
2022-03-22 21:28:41 +0300

    add post model

--- post/models.py
+++ post/models.py
@@ -12,3 +12,22 @@ def generate_id():
         n = 10
         random = str.ascii_uppercase + str.ascii_lowercase + str.digits
         return ''.join(choice(random) for _ in range(n))
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, related_name='post', on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, max_length=10, default=generate_id)
+    photo = models.FileField(upload_to='posts_photo')
+    caption = models.CharField(max_length=50, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Posts'
+        ordering = ['-date_created', ]
+
+    def __str__(self):
+        return self.slug
+
+    def get_absolute_url(self):
+        return reverse('post:view', kwargs={'slug': self.slug})
