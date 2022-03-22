938f4403cef80583cf2f5c476fbb8fe379e95d46
mohraf <amosngok@gmail.com>
2022-03-22 21:17:28 +0300

    add generate id function

--- post/models.py
+++ post/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+import string as str
+from random import choice
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+# from django.utils.encoding import python_2_unicode_compatible
+
+from account.models import User
+
+
+def generate_id():
+        n = 10
+        random = str.ascii_uppercase + str.ascii_lowercase + str.digits
+        return ''.join(choice(random) for _ in range(n))
