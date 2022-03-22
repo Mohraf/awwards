12cbeee8f28a022a0d3c31d22159c3ec8df5f4bf
mohraf <amosngok@gmail.com>
2022-03-22 19:15:51 +0300

    add detail account view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from __future__ import absolute_import
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.http import HttpResponseRedirect, Http404
+from django.views import generic
+from django.urls import reverse_lazy
+
+from braces import views
+
+from .models import User, Connection
+from .forms import SignUpForm, UpdateAccountForm, LoginForm, ChangePasswordForm
+from .helpers import get_current_user
+
+
+class DetailAccountView(
+        views.LoginRequiredMixin,
+        generic.DetailView
+):
+    model = User
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    template_name = 'accounts/account_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailAccountView, self).get_context_data(**kwargs)
+        username = self.kwargs['username']
+        context['username'] = username
+
+        context['user'] = get_current_user(self.request)
+
+        return 
